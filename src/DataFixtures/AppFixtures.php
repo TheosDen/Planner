455ba4bb56c0bc0ee57e91678d653e65b6cb9bca
theos.den@gmail.com <ltybc3458>
2018-09-09 16:55:21 +0300

    Added User and basic authentication

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    /**
+     * AppFixtures constructor.
+     * @param UserPasswordEncoderInterface $encoder
+     */
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+
+        $user
+            ->setUsername('admin')
+            ->setEmail('theos.den@gmail.com');
+
+        $password = $this->encoder->encodePassword($user, '123456');
+        $user->setPassword($password);
+
+        $manager->persist($user);
+        $manager->flush();
+
+    }
+}
